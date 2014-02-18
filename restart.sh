8e3f0ba1b25da8844c4e43e754b6eecbb891642f
Angel pimentel <angelpmza@gmail.com>
2014-02-17 20:48:07 -0600

    restart script

--- restart.sh
+++ restart.sh
@@ -1,2 +1,2 @@
-kill -TERM $(cat target/universal/stage/RUNNING_PID)
+[ -f target/universal/stage/RUNNING_PID ] && kill -TERM $(cat target/universal/stage/RUNNING_PID) || echo "File does not exists"
 nohup ./target/universal/stage/bin/hello_world &
